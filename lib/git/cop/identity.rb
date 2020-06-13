4973412077a9b7bbc6b210a9060c60824d44958d
Brooke Kuhlmann <brooke@alchemists.io>
2020-06-13 15:11:19 -0600

    Added version release notes

--- lib/git/cop/identity.rb
+++ lib/git/cop/identity.rb
@@ -6,7 +6,7 @@ module Git
     module Identity
       NAME = "git-cop"
       LABEL = "Git Cop"
-      VERSION = "4.1.1"
+      VERSION = "4.2.0"
       VERSION_LABEL = "#{LABEL} #{VERSION}"
     end
   end
